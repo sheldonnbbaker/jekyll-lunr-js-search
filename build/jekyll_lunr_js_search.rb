f463df5ad01a161abb4c41123891935b4083c5b0
Sheldon Baker <sheldonnbbaker@gmail.com>
2017-01-09 20:47:35 -0800

    cvxvcxvcx

--- build/jekyll_lunr_js_search.rb
+++ build/jekyll_lunr_js_search.rb
@@ -82,7 +82,9 @@ module Jekyll
           @index.add(doc)
           doc.delete("body")
           @docs[i] = doc
-
+          
+          Jekyll.logger.debug "WTF"
+          Jekyll.logger.debug entry.collection
           Jekyll.logger.debug "Lunr:", (entry.title ? "#{entry.title} (#{entry.url})" : entry.url)
         end
 
